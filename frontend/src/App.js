e53985aedc6fc097e52264969714e1cfd0c52d84
QueirozMirella <queiroz.av@gmail.com>
2020-04-21 17:30:24 -0300

    aprendendo sobre estado e imutabilidade

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  function increment(){
+    setCounter(counter + 1);
+
+  }
+
   return (
-    <Header>
-      Semana Omnistack
-    </Header>
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
   );
 }
 
